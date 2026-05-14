2652741447b5a1802b4175a450b23176abcb0694
YamamotoHimeka <tribestaff0113@gmail.com>
2026-05-14 16:17:03 +0900

    レスポンシブ調節

--- recruit/components/standard.php
+++ recruit/components/standard.php
@@ -12,7 +12,8 @@
           <img src="<?php echo $path; ?>assets/img/girl.png" alt="採用基準について">
         </div>
         <div class="standard__img-2 rellax" data-rellax-speed="-1" data-rellax-percentage="0.5" data-rellax-xs-speed="0" data-rellax-mobile-speed="0">
-          <img src="<?php echo $path; ?>assets/img/comment.png" alt="採用基準について">
+          <img src="<?php echo $path; ?>assets/img/comment.png" alt="採用基準について" class="pc">
+          <img src="<?php echo $path; ?>assets/img/comment-sp.png" alt="採用基準について" class="tab">
         </div>
       </div>
     </div>
